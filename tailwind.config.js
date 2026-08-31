5aa97436ec7866d67f73d88a2b02db78e9273f09
Siaman-utd <axadsiam@gmail.com>
2026-08-31 17:26:17 +0600

    add explicit ui data and lib folders to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./data/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
